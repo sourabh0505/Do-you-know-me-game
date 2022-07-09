1ef2d5448a613ec5799bf0b8dfdcbe1b0927c03f
sourabh vishnoi <>
2022-07-09 12:15:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+var readlineSync = require('readline-sync');
+
+var userName = readlineSync.question('May I have your name?');
+// console.log(userName);
+
+function welcome(){
+  var welcomeMessage = "Welcome! " + userName + ' to Do you know Sourabh?';
+console.log(welcomeMessage);
+}
+
+var score = 0;
+
+function playTheGame(question,answer){
+  var userAnswer = readlineSync.question(question);
+
+  if(userAnswer === answer){
+    console.log('Yay! your answer is correct.');
+    score++;
+  }
+  else{
+    console.log("Oops! your answer is wrong.");
+    score = score;
+  }
+    console.log('Your score is: '+score);
+    console.log('------------------')
+}
+
+var questions = [{
+  question: 'Is sourabh is the only child ?',
+  answer: 'no'
+  },
+  {
+  question: ' At which school did sourabh completed his 10th standard?',
+  answer: 'central academy'
+  },
+  {
+  question: 'In which college sourabh is studying?',
+  answer: 'poornima college of engineering'
+  },
+  {
+  question: 'What is the thing sourabh is crazy about?',
+  answer: 'bikes'
+  },
+  {
+  question: 'Which sport did  sourabh like the most?',
+  answer: 'snooker'
+  }]
+
+function game(){
+  for(var i = 0;i<questions.length;i++){
+  var currentquestion = questions[i];
+  playTheGame(currentquestion.question, currentquestion.answer);
+}
+}
+
+function showScores(){
+  console.log('Yay! you scored: ' + score);
+}
+
+welcome();
+game();
+showScores();
